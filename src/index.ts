1c9d61b06cab1950ccfa37e9e9e5d7a724d70b0d
Florian <sicilia.florian02@gmail.com>
2024-01-07 16:49:20 +0100

    chore: modification index.ts (import/export)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import "@components";
-import "@types";
+// import "@components";
+// import "@types";
+export type { ButtonProps, ChildrenProps, ModalProps } from "@types";
+
+export * from "@components/Button";
+export * from "@components/icons/Close";
+
+import { Modal } from "@components/Modal";
+export default Modal;
+
 import "index.css";
